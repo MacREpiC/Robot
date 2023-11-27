f9713816bb42fe178a9beb2b7dbb3a3598b7ce98
Mac <80626286+MacREpiC@users.noreply.github.com>
2023-11-27 12:46:57 +0100

    rf

--- src/main/Java/org.iesalandalus.programacion.robot/modelo/ControladorRobot.java
+++ src/main/Java/org.iesalandalus.programacion.robot/modelo/ControladorRobot.java
@@ -1,12 +1,13 @@
 package org.iesalandalus.programacion.robot.modelo;
 
 import javax.naming.OperationNotSupportedException;
+import java.util.Objects;
 
 public class ControladorRobot {
     private Robot robot;
 
     public ControladorRobot(Robot robot) {
-        this.robot = new Robot(robot);
+        this.robot = new Robot(Objects.requireNonNull(robot, "El robot no puede ser nulo."));
     }
 
     public Robot getRobot() {
